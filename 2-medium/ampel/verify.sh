2f817a5d02e09fb6b7ff8a317267f4a948fc6054
Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>
2026-03-19 01:21:03 -0600

    Add wild policy and script
    Signed-off-by: Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>

--- 2-medium/ampel/verify.sh
+++ 2-medium/ampel/verify.sh
@@ -2,20 +2,28 @@
 
 set -euo pipefail
 
-mkdir output || :
+if [ -n "${AMPEL:-}" ]; then
+  AMPEL="$AMPEL"
+elif command -v ampel &>/dev/null; then
+  AMPEL="ampel"
+else
+  AMPEL="../carabiner/ampel/ampel"
+fi
 
-ampel verify "$(crane manifest ghcr.io/puerco/mild-to-wild-samples | jq -r '.annotations["org.opencontainers.image.base.digest"]')" \
-   -p 1-mild/ampel/verify-base.hjson \
-   -c coci:registry.access.redhat.com/ubi10/ubi-minimal:latest \
-   -x 'buildPoint:git+https://gitlab.com/redhat/rhel/containers/ubi10-minimal.git' \
+mkdir -p output 2>/dev/null
+
+"$AMPEL" verify "$(crane manifest ghcr.io/puerco/mild-to-wild-samples | jq -r '.annotations["org.opencontainers.image.base.digest"]')" \
+   --policy 1-mild/ampel/policy.hjson \
+   --collector coci:registry.access.redhat.com/ubi10/ubi-minimal:latest \
+   --context 'buildPoint:git+https://gitlab.com/redhat/rhel/containers/ubi10-minimal.git' \
    --attest-format vsa \
    --attest-results=true \
    --results-path=output/base.vsa.json
 
-ampel verify $(crane digest  ghcr.io/puerco/mild-to-wild-samples) \
-    -c oci:ghcr.io/puerco/mild-to-wild-samples \
-    -p 2-medium/ampel/verify-build.hjson \
-    -a output/base.vsa.json \
+"$AMPEL" verify "$(crane digest ghcr.io/puerco/mild-to-wild-samples)" \
+    --collector oci:ghcr.io/puerco/mild-to-wild-samples \
+    --policy 2-medium/ampel/policy.hjson \
+    --attestation output/base.vsa.json \
     --attest-format vsa \
     --attest-results=true \
     --results-path=output/image.vsa.json
