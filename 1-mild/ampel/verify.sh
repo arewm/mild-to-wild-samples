17a43862595b99631dc0dcad0cafcf168b114e3d
Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>
2026-03-23 08:54:59 +0100

    Pin to hash
    Signed-off-by: Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>

--- 1-mild/ampel/verify.sh
+++ 1-mild/ampel/verify.sh
@@ -15,7 +15,8 @@ mkdir -p output/mild/ampel 2>/dev/null
 
 "$AMPEL" verify "$(crane manifest ghcr.io/puerco/mild-to-wild-samples | jq -r '.annotations["org.opencontainers.image.base.digest"]')" \
    --policy 1-mild/ampel/policy.hjson \
-   --collector coci:registry.access.redhat.com/ubi10/ubi-minimal:latest \
+   --collector coci:registry.access.redhat.com/ubi10/ubi-minimal@sha256:734d6c22b80cdf9bd21c6b13d3475cf04d46f131cdb4b32a88cc96c40c6feab1 \
+ \
    --context 'buildPoint:git+https://gitlab.com/redhat/rhel/containers/ubi10-minimal.git' \
    --attest-format vsa \
    --attest-results=true \
